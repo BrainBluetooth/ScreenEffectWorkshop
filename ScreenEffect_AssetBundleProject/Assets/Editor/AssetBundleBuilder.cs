7cc3c7f0bc9ba041d1d1f957e6c254038b938bcb
BrainBluetooth <hanjisung686@gmail.com>
2024-03-25 13:33:06 +0900

    20240325_Init

---
+++ ScreenEffect_AssetBundleProject/Assets/Editor/AssetBundleBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using UnityEditor;
+
+public static class AssetBundleBuilder
+{
+    [MenuItem("Assets/Build Asset Bundles")]
+    public static void BuildAssetBundles()
+    {
+        Directory.CreateDirectory("AssetBundles");
+        BuildPipeline.BuildAssetBundles("AssetBundles", BuildAssetBundleOptions.StrictMode, BuildTarget.StandaloneWindows);
+    }
+}
---
+++ ScreenEffect_AssetBundleProject/Assets/Scenes/CaptureScreenshot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+internal sealed class CaptureScreenshot : MonoBehaviour
+{
+    [SerializeField] private Material imageEffect;
+
+#if UNITY_EDITOR
+    [MenuItem("Edit/Capture")]
+    private static void Capture()
+    {
+        ScreenCapture.CaptureScreenshot("Screen.png");
+    }
+#endif
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (imageEffect != null)
+            Graphics.Blit(source, destination, this.imageEffect);
+        else
+            Graphics.Blit(source, destination);
+    }
+}
